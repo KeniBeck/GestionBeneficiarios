244c50f30d2f9392ae5f33098130e47614c9fcfd
r=cos(4θ) <olvadis2004@gmail.com>
2023-12-02 22:09:57 -0500

    Se realizo la  interfaz principal

---
+++ src/main/java/com/datastructure/code/gestionbeneficiarios/StartController.java
@@ -0,0 +1,16 @@
+package com.datastructure.code.gestionbeneficiarios;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class StartController {
+
+
+
+    public void initialize() {
+
+    }
+}
