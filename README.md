a442b49ac4e5943f3c223b58bb618d795b78134c
r=cos(4θ) <olvadis2004@gmail.com>
2023-12-02 20:53:44 -0500

    I upload de system architect

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# GestionBeneficiarios" 
+This Project is about a development
+of a software that can be done CRUD of
+a beneficiary of government program.
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.datastructure.code.gestionbeneficiarios {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.datastructure.code.gestionbeneficiarios to javafx.fxml;
+    exports com.datastructure.code.gestionbeneficiarios;
+}
